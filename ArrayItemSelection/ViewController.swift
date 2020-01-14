487b2999717dc06ed53fe54368de68f8322be45f
Courtney Hay <courtneyhay@student.fullsail.edu>
2020-01-13 22:51:14 -0500

    add all the buttons func to the view controller

--- ArrayItemSelection/ViewController.swift
+++ ArrayItemSelection/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    var fruits = ["Star Apple", "Jack Fruit", "Genep", "Pine Apple"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func Item1(_ sender: Any) {
+        label.text = fruits[0]
+    }
+    
+    @IBAction func Item2(_ sender: Any) {
+        label.text = fruits[1]
+    }
+    
+    @IBAction func Item3(_ sender: Any) {
+        label.text = fruits[2]
+    }
+    @IBAction func Item4(_ sender: Any) {
+        label.text = fruits[3]
+    }
 }
 
